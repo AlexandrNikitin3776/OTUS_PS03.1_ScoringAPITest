abe11ab9cabb6c8500fc48f89458048803fb83e0
Alexander Nikitin <snegopad1@gmail.com>
2020-06-13 19:44:08 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 12 22:13:07 2020
+
+@author: alex
+"""
+
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scoringapi", # Replace with your own username
+    version="0.0.1",
+    author="AlexandrNikitin3776",
+    author_email="snegopad1@gmail.com",
+    description="scoring API package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AlexandrNikitin3776/OTUS_PS03_ScoringAPI",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
