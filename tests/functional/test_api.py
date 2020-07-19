355fe72f337a46c4b1f11ed489d84e1f12de1da9
Alexander Nikitin <snegopad1@gmail.com>
2020-07-19 21:50:58 +0300

    added mock store

--- tests/functional/test_api.py
+++ tests/functional/test_api.py
@@ -1,53 +1,57 @@
 import hashlib
 import datetime
-# import functools
 import pytest
 
 from scoringapi import api
 from scoringapi.store import Store
 
 
-class TestSuite:
-    def setup(self):
-        self.context = {}
-        self.headers = {}
-        self.settings = Store('store')
+context = {}
+headers = {}
+settings = Store('store')
 
-    def get_response(self, request):
-        return api.method_handler({"body": request, "headers": self.headers}, self.context, self.settings)
 
-    def set_valid_auth(self, request):
-        if request.get("login") == api.ADMIN_LOGIN:
-            msg = datetime.datetime.now().strftime("%Y%m%d%H") + api.ADMIN_SALT
-            request["token"] = hashlib.sha512(msg.encode('utf-8')).hexdigest()
-        else:
-            msg = request.get("account", "") + request.get("login", "") + api.SALT
-            request["token"] = hashlib.sha512(msg.encode('utf-8')).hexdigest()
+def get_response(request):
+    return api.method_handler({"body": request, "headers": headers}, context, settings)
 
-    def test_empty_request(self):
-        _, code = self.get_response({})
-        assert api.INVALID_REQUEST == code
 
-    @pytest.mark.parametrize('req', [
-        {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "token": "", "arguments": {}},
-        {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "token": "sdd", "arguments": {}},
-        {"account": "horns&hoofs", "login": "admin", "method": "online_score", "token": "", "arguments": {}},
-    ])
-    def test_bad_auth(self, req):
-        _, code = self.get_response(req)
-        assert api.FORBIDDEN == code
+def set_valid_auth(request):
+    if request.get("login") == api.ADMIN_LOGIN:
+        msg = datetime.datetime.now().strftime("%Y%m%d%H") + api.ADMIN_SALT
+        request["token"] = hashlib.sha512(msg.encode('utf-8')).hexdigest()
+    else:
+        msg = request.get("account", "") + request.get("login", "") + api.SALT
+        request["token"] = hashlib.sha512(msg.encode('utf-8')).hexdigest()
 
-    @pytest.mark.parametrize('req', [
-        {"account": "horns&hoofs", "login": "h&f", "method": "online_score"},
-        {"account": "horns&hoofs", "login": "h&f", "arguments": {}},
-        {"account": "horns&hoofs", "method": "online_score", "arguments": {}},
-    ])
-    def test_invalid_method_request(self, req):
-        self.set_valid_auth(req)
-        response, code = self.get_response(req)
-        assert api.INVALID_REQUEST == code
-        assert len(response)
 
+def test_empty_request():
+    _, code = get_response({})
+    assert api.INVALID_REQUEST == code
+
+
+@pytest.mark.parametrize('req', [
+    {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "token": "", "arguments": {}},
+    {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "token": "sdd", "arguments": {}},
+    {"account": "horns&hoofs", "login": "admin", "method": "online_score", "token": "", "arguments": {}},
+])
+def test_bad_auth(req):
+    _, code = get_response(req)
+    assert api.FORBIDDEN == code
+
+
+@pytest.mark.parametrize('req', [
+    {"account": "horns&hoofs", "login": "h&f", "method": "online_score"},
+    {"account": "horns&hoofs", "login": "h&f", "arguments": {}},
+    {"account": "horns&hoofs", "method": "online_score", "arguments": {}},
+])
+def test_invalid_method_request(req):
+    set_valid_auth(req)
+    response, code = get_response(req)
+    assert api.INVALID_REQUEST == code
+    assert len(response)
+
+
+class TestScoreRequest:
     @pytest.mark.parametrize('arguments', [
         {},
         {"phone": "79175002040"},
@@ -65,8 +69,8 @@ class TestSuite:
     ])
     def test_invalid_score_request(self, arguments):
         request = {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "arguments": arguments}
-        self.set_valid_auth(request)
-        response, code = self.get_response(request)
+        set_valid_auth(request)
+        response, code = get_response(request)
         assert api.INVALID_REQUEST == code
         assert len(response)
 
@@ -82,22 +86,36 @@ class TestSuite:
     ])
     def test_ok_score_request(self, arguments):
         request = {"account": "horns&hoofs", "login": "h&f", "method": "online_score", "arguments": arguments}
-        self.set_valid_auth(request)
-        response, code = self.get_response(request)
+        set_valid_auth(request)
+        response, code = get_response(request)
         assert api.OK == code
         score = response.get("score")
         assert isinstance(score, (int, float)) and score >= 0, arguments
-        assert sorted(self.context["has"]) == sorted(arguments.keys())
+        assert sorted(context["has"]) == sorted(arguments.keys())
 
     def test_ok_score_admin_request(self):
         arguments = {"phone": "79175002040", "email": "stupnikov@otus.ru"}
         request = {"account": "horns&hoofs", "login": "admin", "method": "online_score", "arguments": arguments}
-        self.set_valid_auth(request)
-        response, code = self.get_response(request)
+        set_valid_auth(request)
+        response, code = get_response(request)
         assert api.OK == code
         score = response.get("score")
         assert score == 42
 
+
+class MockStore:
+    def get(self):
+        interests = ["cars", "pets", "travel", "hi-tech", "sport", "music",
+                     "books", "tv", "cinema", "geek", "otus"]
+        return random.sample(interests, 2)
+
+
+@pytest.fixture
+def set_mock_store():
+    settings = MockStore()
+
+
+class TestInterestsRequest:
     @pytest.mark.parametrize('arguments', [
         {},
         {"date": "20.07.2017"},
@@ -106,10 +124,10 @@ class TestSuite:
         {"client_ids": ["1", "2"], "date": "20.07.2017"},
         {"client_ids": [1, 2], "date": "XXX"},
     ])
-    def test_invalid_interests_request(self, arguments):
+    def test_invalid_interests_request(self, arguments, set_mock_store):
         request = {"account": "horns&hoofs", "login": "h&f", "method": "clients_interests", "arguments": arguments}
-        self.set_valid_auth(request)
-        response, code = self.get_response(request)
+        set_valid_auth(request)
+        response, code = get_response(request)
         assert api.INVALID_REQUEST == code
         assert len(response)
 
@@ -118,12 +136,12 @@ class TestSuite:
         {"client_ids": [1, 2], "date": "19.07.2017"},
         {"client_ids": [0]},
     ])
-    def test_ok_interests_request(self, arguments):
+    def test_ok_interests_request(self, arguments, set_mock_store):
         request = {"account": "horns&hoofs", "login": "h&f", "method": "clients_interests", "arguments": arguments}
-        self.set_valid_auth(request)
-        response, code = self.get_response(request)
+        set_valid_auth(request)
+        response, code = get_response(request)
         assert api.OK == code
         assert len(arguments["client_ids"]) == len(response)
         assert all(v and isinstance(v, list) and all(isinstance(i, (bytes, str)) for i in v)
                    for v in response.values())
-        assert self.context.get("nclients") == len(arguments["client_ids"])
+        assert context.get("nclients") == len(arguments["client_ids"])
